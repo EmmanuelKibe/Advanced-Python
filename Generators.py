ceef078ed1db23f321e329bc7fdf9c9819be001e
Emmanuel <emmanuelkibe148@gmail.com>
2025-11-14 19:18:58 +0300

    Add normal_count_up_to function to return a list of numbers

--- Generators.py
+++ Generators.py
@@ -7,3 +7,13 @@ def count_up_to(n):
 numbers = count_up_to(5)
 for number in numbers:
     print(number)
+
+#Normal function
+def normal_count_up_to(n):
+    result = []
+    count = 1
+    while count <= n:
+        result.append(count)
+        count += 1
+    return result
+print(normal_count_up_to(5))
