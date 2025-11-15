0492d56a393e7151fa00eb00c2c3551c1f669af5
Emmanuel <emmanuelkibe148@gmail.com>
2025-11-15 11:23:56 +0300

    Code showing the asynchronous version

--- asynchronous_prog.py
+++ asynchronous_prog.py
@@ -1,11 +1,28 @@
-#A normal synchronous function
+""" #A normal synchronous function
 import time
 
 def fetch_data():
     time.sleep(3)  # Pretend to fetch something
     return "Data loaded"
 
-print("Start")
+#print("Start")
 data = fetch_data()   # Everything stops here for 3 seconds
-print(data)
-print("End")
+#print(data)
+#print("End") """
+
+#An asynchronous version
+import asyncio
+
+async def fetch_data():
+    await asyncio.sleep(3)  # Non-blocking wait
+    return "Data loaded"
+
+async def main():
+    print("Start")
+    task = asyncio.create_task(fetch_data())  # Start task in background
+    print("Doing other work...")
+    data = await task  # Come back when ready
+    print(data)
+    print("End")
+
+asyncio.run(main())
